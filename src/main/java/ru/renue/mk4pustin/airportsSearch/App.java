07e1884b445e856a55f9636f24b6e4c00444302b
Maxim Kapustin <mk4pustin@gmail.com>
2023-05-07 14:41:13 +0300

    Initial commit

---
+++ src/main/java/ru/renue/mk4pustin/airportsSearch/App.java
@@ -0,0 +1,13 @@
+package ru.renue.mk4pustin.airportsSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
